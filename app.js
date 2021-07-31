ba0f4cb359f68eba3d3f04f700795f6a5de5199a
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-07-31 16:06:53 -0600

    get request setup for api

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
-
+const fs = require('fs')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('in server side')
-} )
-
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'Success',
+        tours: tours.length,
+        data: {
+            tours
+        }
+    })
+})
 const port = 3000
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
