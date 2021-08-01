ecd4538120512c2817a39a7ec02da110a1d2810f
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-07-31 18:31:22 -0600

    clean up + refactoring

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
 const express = require('express')
-const fs = require('fs')
+const morgan = require('morgan')
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 const app = express()
+
+// MIDDLEWARE
 app.use(express.json())
+app.use(morgan('dev'))
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-app.get('/api/v1/tours', (req,res) => {
-    res.status(200).json({
-        status: 'Success',
-        tours: tours.length,
-        data: {
-            tours
-        }
-    })
-})
+// ROUTES
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', tourRouter)
 
-app.post('/api/v1/tours', (req, res) => {
-    const id = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({id: id}, req.body)
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'Success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-})
-
-const port = 3000
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
-
+module.exports = app
