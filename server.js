402a5ea6da238d441b0d3a0fc20acdcd09046aa3
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-08-05 22:41:53 -0600

    setup db connection to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const app = require('./app')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const app = require('./app')
 
-dotenv.config({ path: './congif.env'})
+dotenv.config({ path: './config.env'})
 
-console.log(process.env)
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+//MONGOOSE CONNECTION 
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('db connection success!')
+})
+
 //SERVER START
 const port = process.env.PORT || 3000
 app.listen(port, () => {
