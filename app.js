08ed22caaeb1d92e9186fed39408c93cad9d711d
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-08-28 09:17:13 -0600

    adding error handling middleware

--- app.js
+++ app.js
@@ -16,9 +16,24 @@ app.use('/api/v1/users', tourRouter)
 
 //error handling for undefined routes
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status:'failed',
-        message: `${req.originalUrl} does not exsist`
+    // res.status(404).json({
+    //     status:'failed',
+    //     message: `${req.originalUrl} does not exsist`
+    // })
+    const err = new Error(`${req.originalUrl} does not exsist`)
+    err.status = 'fail'
+    err.statusCode = 404
+
+    next(err)
+})
+
+//error middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     })
 })
 
